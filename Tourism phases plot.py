dbe965218c7d195f6274dbac59889f7742d21f62
dsammanthan <92326520+dsammanthan@users.noreply.github.com>
2023-02-09 00:09:28 +0100

    Plot visualising the three development phases of tourism
    Plot is purely for visual demonstration purposes. Theory taken from: Foye, C. (2016) “The relationship between size of living space and subjective well-being,” Journal of Happiness Studies, 18(2), pp. 427–461. Available at: https://doi.org/10.1007/s10902-016-9732-2

---
+++ Tourism phases plot.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[46]:
+
+
+# Import our modules that we are using
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Create the vectors X and Y
+x1 = np.arange(1.5*np.pi,1.86*np.pi,0.02)   # start,stop,step
+y1 = np.sin(x1)
+x2 = np.arange(1.85*np.pi,2.15*np.pi,0.02)   # start,stop,step
+y2 = np.sin(x2)
+x3 = np.arange(2.15*np.pi,2.5*np.pi,0.02)   # start,stop,step
+y3 = np.sin(x3)
+
+# Create the plot
+plt.plot(x1,y1,label='Pioneering phase')
+plt.plot(x2,y2,label='Mature phase')
+plt.plot(x3,y3,label='Mass phase')
+
+# Add a title
+plt.title('The three phases of space tourism')
+
+# Add X and y Label
+plt.xlabel('Time')
+plt.ylabel('Number of tourists')
+plt.xticks([])
+plt.yticks([])
+
+# Add a Legend
+plt.legend()
+
+# Show the plot
+plt.show()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
